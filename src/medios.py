06d10fefee9721b63d6bb7208f6726118ac10ceb
Francisco Rivera <elpacko@gmail.com>
2023-03-08 23:45:18 -0700

    Se agrega el notebook de polipos

--- src/medios.py
+++ src/medios.py
@@ -17,7 +17,8 @@ capturas_localizaciones = os.path.join(
 )
 progressbar_settings = [progressbar.Bar("=", "[", "]"), " ", progressbar.Percentage()]
 file_exists_path = os.path.join(capturas_root, "file_exists_status.csv")
-
+full_categorias_subfolder = "EstudiosSeparados"
+polipos_categorias_subfolder = "EstudiosPolipos"
 
 def __init__():
     pass
@@ -32,8 +33,10 @@ def get_capturas_localizaciones_df():
 
 
 def get_medios_path():
-    return os.path.join(capturas_root, "EstudiosSeparados/")
+    return os.path.join(capturas_root, f"{full_categorias_subfolder}/")
 
+def get_medios_polipos_path():
+    return os.path.join(capturas_root, f"{polipos_categorias_subfolder}/")
 
 def init_categorias_folders(target_folder):
     print("Creando folders de categorias")
@@ -104,10 +107,9 @@ def get_file_exists():
 def separar_imagenes_localizaciones():
     from shutil import copyfile
 
-    target_folder = "EstudiosSeparados"
     capturas_df = get_file_exists()
     capturas_df = capturas_df.loc[capturas_df["file_exists_status"] == True]
-    init_categorias_folders(target_folder)
+    init_categorias_folders(full_categorias_subfolder)
     bar = progressbar.ProgressBar(
         maxval=capturas_df.shape[0],
         widgets=progressbar_settings,
@@ -116,7 +118,7 @@ def separar_imagenes_localizaciones():
     for index, row in capturas_df.iterrows():
         localizaciones_df = get_capturas_localizaciones_df()
         categoria = get_categoria(localizaciones_df, row["DescripcionEnEstudio"])
-        categoria_path = os.path.join(capturas_root, target_folder, str(categoria))
+        categoria_path = os.path.join(capturas_root, full_categorias_subfolder, str(categoria))
         target_path = os.path.join(categoria_path, os.path.basename(row["url"]))
         source_path = os.path.join(capturas_root, row["url"])
         if index % 10 == 0:
@@ -133,8 +135,7 @@ def separar_imagenes_localizaciones():
 def separar_imagenes_polipo_binario():
     capturas_df = get_file_exists()
     capturas_df = capturas_df.loc[capturas_df["file_exists_status"] == True]
-    targetfolder = "EstudiosPolipos"
-    init_categorias_polipos_folders(targetfolder)
+    init_categorias_polipos_folders(polipos_categorias_subfolder)
     bar = progressbar.ProgressBar(
         maxval=capturas_df.shape[0] + 1,
         widgets=progressbar_settings,
@@ -142,7 +143,7 @@ def separar_imagenes_polipo_binario():
     bar.start()
     for index, row in capturas_df.iterrows():
         categoria = get_tiene_polipo(row["DescripcionEnEstudio"])
-        categoria_path = os.path.join(capturas_root, targetfolder, str(categoria))
+        categoria_path = os.path.join(capturas_root, polipos_categorias_subfolder, str(categoria))
         target_path = os.path.join(categoria_path, os.path.basename(row["url"]))
         source_path = os.path.join(capturas_root, row["url"])
         if index % 10 == 0:
@@ -156,10 +157,9 @@ def separar_imagenes_polipo_binario():
     return capturas_df
 
 def select_sample_images():
-    targetfolder = "EstudiosPolipos"
-    target_path = os.path.join(capturas_root, targetfolder, "NOPOLIPO")
-    source_path = os.path.join(capturas_root, targetfolder, "NOPOLIPO_FULL")
-    sample_path = os.path.join(capturas_root, targetfolder, "POLIPO")
+    target_path = os.path.join(capturas_root, polipos_categorias_subfolder, "NOPOLIPO")
+    source_path = os.path.join(capturas_root, polipos_categorias_subfolder, "NOPOLIPO_FULL")
+    sample_path = os.path.join(capturas_root, polipos_categorias_subfolder, "POLIPO")
     files = os.listdir(source_path) # get all files in the folder
     sample_size = len(os.listdir(sample_path))
     sample = random.sample(files, sample_size) # select 3 files randomly
